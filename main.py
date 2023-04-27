1f065a1ba7eeb490ced3f0685abf3e49089f6451
Yan Kochur <mr.uanby@mail.ru>
2023-04-27 20:59:53 +0300

    trying to make the transition to next pages

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ from bs4 import BeautifulSoup as BS
 
 
 url = 'https://re.kufar.by/l/brest/kupit/kvartiru?cnd=2&cur=USD'        # (with filter)
+has_next_page = True        #
+
+while has_next_page:
+    response = requests.get(url)    # get content from url
+    html = BS(response.content, 'html.parser')       # put the content from response
+    ads = html.find_all('a', class_='styles_wrapper__KkryZ')         # search in class_
 
 
-response = requests.get(url)    # get content from url
-html = BS(response.content, 'html.parser')       # put the content from response
-ads = html.find_all('a', class_='styles_wrapper__KkryZ')         # search in class_
+    for ad in ads:
+        adress = ad.find('span', class_='styles_address__dfWQi').text
+        price = ad.find('span', class_='styles_price__byr__FKDpp').text
+        size = ad.find('div', class_='styles_parameters__p2sHq').text
+        print(adress, '|', price, '|', size)
+
+    next_page_link = html.find('a', class_='styles_link__3MFs4 styles_arrow__r6dv_')
 
 
-for ad in ads:
-    adress = ad.find('span', class_='styles_address__dfWQi').text
-    price = ad.find('span', class_='styles_price__byr__FKDpp').text
-    size = ad.find('div', class_='styles_parameters__p2sHq').text
-    print(adress, '|', price, '|', size)
+    if next_page_link:
+        url = 'https://re.kufar.by' + next_page_link['href']
+    else:
+        has_next_page = False
