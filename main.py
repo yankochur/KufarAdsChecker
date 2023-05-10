1f874412af8ee206b16cab9218faa86354ed434d
Yan Kochur <mr.uanby@mail.ru>
2023-05-10 20:24:14 +0300

    Finally finished f*cking function to remove old ads

--- main.py
+++ main.py
@@ -22,28 +22,23 @@ chat_id = config.get('DEFAULT', 'chat_id')
 bot = telebot.TeleBot(bot_token)
 
 def main():
-
-    print(bot_token, chat_id, 'a')
     conn = sqlite3.connect('kufar_ads.db')
     cursor = conn.cursor()
-    # new_database = """ CREATE TABLE IF NOT EXISTS kufar_ads (id INTEGER) """      # create database
-    # cursor.execute(new_database)
-
-    # add_column_in_database = """ ALTER TABLE kufar_ads ADD COLUMN sent INTEGER; """      # create column "sent"
-    # cursor.execute(add_column_in_database)
-
-    # add_column_status_in_database = """ ALTER TABLE kufar_ads ALTER COLUMN sent SET DEFAULT 'False'; """      # create column "sent"
-    # cursor.execute(add_column_status_in_database)
 
     # cursor.execute("DELETE FROM kufar_ads")         # delete all from database
 
-    url = 'https://re.kufar.by/l/brest/kupit/kvartiru?cnd=2&cur=USD&gbx=b%3A23.605041639404277%2C51.84106358705331%2C24.01702894409179%2C52.1880572948453&oph=1&size=30'        # (with filter)
+    url = 'https://re.kufar.by/l/brest/kupit/kvartiru?cnd=2&cur=USD&oph=1'
     has_next_page = True
     started = False
+
+    cursor.execute("SELECT id FROM kufar_ads")
+    database_content = set(row[0] for row in cursor.fetchall())
+    ids_on_page = set()
+
     while has_next_page:
         response = requests.get(url)    # get content from url
         html = BS(response.content, 'html.parser')       # put the content from response
-        ads = html.find_all('a', class_='styles_wrapper__KkryZ')         # search in class_
+        ads = html.find_all('a', class_='styles_wrapper__KkryZ')
 
         for ad in ads:
             photo = ad.find('img', class_='styles_image--blur__qdvGq lazyload')['data-src']
@@ -56,42 +51,57 @@ def main():
             # print(full_info)
             # print(photo)
 
+
             select_query1 = """ SELECT * FROM kufar_ads WHERE id = ?; """
             cursor.execute(select_query1, (link_id,))
             result = cursor.fetchone()
+            ads_on_page.add(link_id)
 
             if not result:
-                ins_query = """ INSERT INTO kufar_ads (id, sent) VALUES (?, ?); """       # add values into db
-                bot.send_photo(chat_id, photo, full_info, parse_mode='html')            # A request to the Telegram API was unsuccessful. Error code: 404. Description: Not Found
+                ins_query = """ INSERT INTO kufar_ads (id, sent) VALUES (?, ?); """
+                bot.send_photo(chat_id, photo, full_info, parse_mode='html')
                 print(full_info)
                 cursor.execute(ins_query, (link_id, 1))
                 conn.commit()
+                ads_on_page.add(link_id)
 
-            select_query2 = """SELECT * FROM kufar_ads WHERE sent=1"""
-            cursor.execute(select_query2)
-            result = cursor.fetchall()
-
-            for row in result:
-                # bot.send_photo(chat_id, photo, full_info, parse_mode='html')
-                print(full_info)
-                update_query = """UPDATE kufar_ads SET sent=0 WHERE id=?"""
-                cursor.execute(update_query, (row[0],))
-                conn.commit()                                    #  this code incorrect
 
         arrows = html.find_all('a', class_='styles_link__3MFs4 styles_arrow__r6dv_')        # step on next pages
-        if len(arrows) == 1:                # if links 1 unit
-            next_page_link = arrows[0]      # take first[0] link
-            if started is False:
-                started = True              # and start script
-            else:
-                pass
+        if len(arrows) == 1:
+            next_page_link = arrows[0]
+            if started is False:        #
+                started = True          #
+
+            else:                       #
+                break                   # break cycle on last page
+
         else:
-            next_page_link = arrows[1]      # else take second[1] link
+            next_page_link = arrows[1]
 
         if next_page_link:
             url = 'https://re.kufar.by' + next_page_link['href']
         else:
             has_next_page = False
 
+    # ids_to_remove = database_content.intersection(ids_on_page)
+
+    def delete_old_ads
+        ads_on_page = set(map(int, ads_on_page))
+        ads_to_remove = database_content - ads_on_page
+        print(database_content)
+        print(ads_on_page)
+        print(ads_to_remove)
+
+        delete_query = """DELETE FROM kufar_ads WHERE id IN ({})""".format(','.join("'" + str(id) + "'" for id in ads_to_remove))
+        cursor.execute(delete_query)
+        conn.commit()
+
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
